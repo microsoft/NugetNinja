2658449a6edb89a92f479fe235ac984f14147134
Anduin Xue <xuef@microsoft.com>
2022-07-31 16:55:10 +0800

    One command to run all

---
+++ src/NugetNinja.AllOfficialsPlugin/AllOfficialsPlugin.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.NugetNinja.Core;
+
+namespace Microsoft.NugetNinja.AllOfficialsPlugin;
+
+public class AllOfficialsPlugin : INinjaPlugin
+{
+    public CommandHandler Install()
+    {
+        return new AllOfficialsHandler();
+    }
+}
