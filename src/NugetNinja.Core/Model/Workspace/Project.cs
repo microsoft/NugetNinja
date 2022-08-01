714574262c0f7f0d09773a02ccfa403ea025ef8c
Anduin Xue <xuef@microsoft.com>
2022-08-01 18:40:21 +0800

    Correctly remove.

--- src/NugetNinja.Core/Model/Workspace/Project.cs
+++ src/NugetNinja.Core/Model/Workspace/Project.cs
@@ -28,6 +28,8 @@ public class Project
         var csprojContent = await File.ReadAllTextAsync(PathOnDisk);
         var doc = new HtmlDocument();
         doc.OptionOutputOriginalCase = true;
+        doc.OptionAutoCloseOnEnd = true;
+        doc.OptionWriteEmptyNodes = true;
         doc.LoadHtml(csprojContent);
         var node = doc.DocumentNode
             .Descendants("PackageReference")
@@ -40,10 +42,9 @@ public class Project
         }
 
         await this.RemoveNode(node, doc, PathOnDisk);
-        // to do: Remove in itself.
     }
 
-    public async Task RemoveProjectReference(string refName)
+    public async Task RemoveProjectReference(string absPath)
     {
         var csprojContent = await File.ReadAllTextAsync(PathOnDisk);
         var contextPath = Path.GetDirectoryName(PathOnDisk) ?? throw new IOException($"Couldn't find the project path based on: '{PathOnDisk}'.");
@@ -52,25 +53,36 @@ public class Project
         doc.LoadHtml(csprojContent);
         var node = doc.DocumentNode
             .Descendants("ProjectReference")
-            .Where(p => Path.Equals(refName, StringExtensions.GetAbsolutePath(contextPath, p.Attributes["Include"].Value)))
+            .Where(p => Equals(absPath, StringExtensions.GetAbsolutePath(contextPath, p.Attributes["Include"].Value)))
             .FirstOrDefault();
 
         if (node == null)
         {
-            throw new InvalidOperationException($"Could remove PackageReference {refName} in project {this} because it was not found!");
+            throw new InvalidOperationException($"Could remove PackageReference {absPath} in project {this} because it was not found!");
         }
 
         await this.RemoveNode(node, doc, PathOnDisk);
-        // to do: Remove in itself.
     }
 
     private async Task RemoveNode(HtmlNode node, HtmlDocument doc, string path)
     {
-        node.Remove();
+        var parent = node.ParentNode;
+        if (!parent.Descendants(0).Where(n => n.NodeType == HtmlNodeType.Element).Except(new HtmlNode[] { node }).Any())
+        {
+            parent.Remove();
+        }
+        else
+        {
+            node.Remove();
+        }
+
         var memoryStream = new MemoryStream();
         doc.Save(memoryStream);
         memoryStream.Seek(0, SeekOrigin.Begin);
         var csprojText = await new StreamReader(memoryStream).ReadToEndAsync();
+        csprojText = csprojText
+            .Replace(@"></PackageReference>", "/>")
+            .Replace(@"></ProjectReference>", "/>");
         await File.WriteAllTextAsync(path, csprojText);
     }
 }
