d8f02ac70a3cd84d8a9f6c89b1dc8a5cdb990e7f
Anduin Xue <xuef@microsoft.com>
2022-08-04 14:44:32 +0800

    Really update.

--- src/NugetNinja.Core/Model/Workspace/Project.cs
+++ src/NugetNinja.Core/Model/Workspace/Project.cs
@@ -23,6 +23,29 @@ public class Project
         return Path.GetFileNameWithoutExtension(PathOnDisk);
     }
 
+    public async Task SetPackageReferenceVersionAsync(string refName, NugetVersion newVersion)
+    {
+        var csprojContent = await File.ReadAllTextAsync(PathOnDisk);
+        var doc = new HtmlDocument();
+        doc.OptionOutputOriginalCase = true;
+        doc.OptionAutoCloseOnEnd = true;
+        doc.OptionWriteEmptyNodes = true;
+        doc.LoadHtml(csprojContent);
+        var node = doc.DocumentNode
+            .Descendants("PackageReference")
+            .Where(d => d.Attributes["Include"].Value == refName)
+            .FirstOrDefault();
+
+        if (node == null)
+        {
+            throw new InvalidOperationException($"Could remove PackageReference {refName} in project {this} because it was not found!");
+        }
+
+        node.Attributes["Version"].Value = newVersion.ToString();
+
+        await this.SaveDocToDisk(doc);
+    }
+
     public async Task RemovePackageReferenceAsync(string refName)
     {
         var csprojContent = await File.ReadAllTextAsync(PathOnDisk);
@@ -41,7 +64,7 @@ public class Project
             throw new InvalidOperationException($"Could remove PackageReference {refName} in project {this} because it was not found!");
         }
 
-        await this.RemoveNode(node, doc, PathOnDisk);
+        await this.RemoveNode(node, doc);
     }
 
     public async Task RemoveProjectReference(string absPath)
@@ -61,10 +84,10 @@ public class Project
             throw new InvalidOperationException($"Could remove PackageReference {absPath} in project {this} because it was not found!");
         }
 
-        await this.RemoveNode(node, doc, PathOnDisk);
+        await this.RemoveNode(node, doc);
     }
 
-    private async Task RemoveNode(HtmlNode node, HtmlDocument doc, string path)
+    private async Task RemoveNode(HtmlNode node, HtmlDocument doc)
     {
         var parent = node.ParentNode;
         if (!parent.Descendants(0).Where(n => n.NodeType == HtmlNodeType.Element).Except(new HtmlNode[] { node }).Any())
@@ -76,6 +99,11 @@ public class Project
             node.Remove();
         }
 
+        await this.SaveDocToDisk(doc);
+    }
+
+    private async Task SaveDocToDisk(HtmlDocument doc)
+    {
         var memoryStream = new MemoryStream();
         doc.Save(memoryStream);
         memoryStream.Seek(0, SeekOrigin.Begin);
@@ -83,6 +111,6 @@ public class Project
         csprojText = csprojText
             .Replace(@"></PackageReference>", "/>")
             .Replace(@"></ProjectReference>", "/>");
-        await File.WriteAllTextAsync(path, csprojText);
+        await File.WriteAllTextAsync(this.PathOnDisk, csprojText);
     }
 }
