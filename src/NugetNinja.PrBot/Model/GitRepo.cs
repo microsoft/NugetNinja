5ddcf575a5e2022c99f56fb165ab4e67c1fbc2ec
Anduin Xue <xuef@microsoft.com>
2022-08-22 23:20:55 +0800

    Create gitrepo model.

---
+++ src/NugetNinja.PrBot/Model/GitRepo.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.NugetNinja.Core;
+
+namespace Microsoft.NugetNinja.PrBot;
+
+public enum RepoProvider
+{
+    GitHub,
+    AzureDevOps
+}
+
+public class GitRepo
+{
+    public GitRepo(
+        string name, 
+        string cloneEndpoint,
+        RepoProvider repoProvider)
+    {
+        Name = name;
+        CloneEndpoint = cloneEndpoint;
+        Provider = repoProvider;
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string CloneEndpoint { get; set; }
+    public string? CloneToken { get; set; }
+
+    public string NugetServer { get; set; } = NugetService.DefaultNugetServer;
+    public string? NugetPatToken { get; set; }
+
+    public RepoProvider Provider { get; set; }
+}
--- src/NugetNinja.PrBot/Program.cs
+++ src/NugetNinja.PrBot/Program.cs
@@ -2,9 +2,18 @@
 // Licensed under the MIT License.
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.NugetNinja.Core;
 
 var services = new ServiceCollection();
+services.AddLogging(logging =>
+{
+    logging
+        .AddFilter("Microsoft.Extensions", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning);
+    logging.AddConsole();
+    logging.SetMinimumLevel(LogLevel.Information);
+});
 services.AddMemoryCache();
 services.AddHttpClient();
 services.AddSingleton<CacheService>();
