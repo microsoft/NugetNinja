48fc006f5ed0786347316100e404f206975df5e0
Anduin Xue <Anduin@aiursoft.com>
2022-08-25 00:54:03 +0800

    More logging.

--- src/NugetNinja.PrBot/Entry.cs
+++ src/NugetNinja.PrBot/Entry.cs
@@ -58,58 +58,73 @@ public class Entry
             await _repoDbContext.SaveChangesAsync();
         }
 
-        foreach (var repo in await _repoDbContext.Repos.ToListAsync())
+        var allRepos = await _repoDbContext.Repos.ToListAsync();
+        _logger.LogInformation($"Got {allRepos.Count} repositories as registered to create pull requests automatically.");
+        foreach (var repo in allRepos)
         {
-            // Get details from GitHub
-            var repoDetails = await _gitHubService.GetRepo(repo.Org, repo.Name);
-
-            // Clone locally.
-            _logger.LogInformation($"Cloning repository: {repo.Name}...");
-            var workPath = Path.Combine(_workspaceFolder, $"workspace-{repo.Name}");
-            await _workspaceManager.ResetRepo(
-                path: workPath,
-                branch: repoDetails.DefaultBranch ?? throw new NullReferenceException($"The default branch of {repoDetails.Name} is null!"),
-                endPoint: repoDetails.CloneUrl ?? throw new NullReferenceException($"The clone endpoint branch of {repoDetails.Name} is null!"));
-
-            // Run all plugins.
-            await _runAllOfficialPluginsService.OnServiceStartedAsync(workPath, true);
-
-            // Consider changes...
-            if (!await _workspaceManager.PendingCommit(workPath))
+            try
             {
-                _logger.LogInformation($"{repo} has no suggestion that we can make. Ignore.");
-                continue;
+                _logger.LogInformation($"Processing repository {repo.Org}/{repo.Name}...");
+                await ProcessRepository(repo);
             }
-            _logger.LogInformation($"{repo} is pending some fix. We will try to create\\update related pull request.");
-            await _workspaceManager.SetUserConfig(workPath, username: _githubUserDisplayName, email: _githubUserEmail);
-            var saved = await _workspaceManager.CommitToBranch(workPath, "Auto csproj fix and update by bot.", branch: _workingBranch);
-            if (!saved)
+            catch (Exception e)
             {
-                _logger.LogInformation($"{repo} has no suggestion that we can make. Ignore.");
-                continue;
+                _logger.LogError(e, $"Crashed when processing repo: {repo.Org}/{repo.Name}!");
             }
+        }
+    }
 
-            // Fork repo.
-            while ((await _gitHubService.GetRepos(_githubUserName)).All(r => r.Name != repo.Name))
-            {
-                await _gitHubService.ForkRepo(repo.Org, repo.Name);
-                // Wait a while. GitHub may need some time to fork the repo.
-                await Task.Delay(3000); 
-            }
+    private async Task ProcessRepository(GitRepo repo)
+    {
+        // Get details from GitHub
+        var repoDetails = await _gitHubService.GetRepo(repo.Org, repo.Name);
 
-            // Push to forked repo.
-            await _workspaceManager.Push(workPath, _workingBranch, $"https://{_githubUserName}:{_githubToken}@github.com/{_githubUserName}/{repo.Name}.git", force: true);
+        // Clone locally.
+        _logger.LogInformation($"Cloning repository: {repo.Name}...");
+        var workPath = Path.Combine(_workspaceFolder, $"workspace-{repo.Name}");
+        await _workspaceManager.ResetRepo(
+            path: workPath,
+            branch: repoDetails.DefaultBranch ?? throw new NullReferenceException($"The default branch of {repoDetails.Name} is null!"),
+            endPoint: repoDetails.CloneUrl ?? throw new NullReferenceException($"The clone endpoint branch of {repoDetails.Name} is null!"));
 
-            var existingPullRequestsByBot = await _gitHubService.GetPullRequest(repo.Org, repo.Name, head: $"{_githubUserName}:{_workingBranch}");
-            if (existingPullRequestsByBot.All(p => p.State != "open"))
-            {
-                // Create a new pull request.
-                await _gitHubService.CreatePullRequest(repo.Org, repo.Name, head: $"{_githubUserName}:{_workingBranch}", @base: repoDetails.DefaultBranch);
-            }
-            else
-            {
-                _logger.LogInformation($"Skipped creating new pull request for {repo} because there already exists.");
-            }
+        // Run all plugins.
+        await _runAllOfficialPluginsService.OnServiceStartedAsync(workPath, true);
+
+        // Consider changes...
+        if (!await _workspaceManager.PendingCommit(workPath))
+        {
+            _logger.LogInformation($"{repo} has no suggestion that we can make. Ignore.");
+            return;
+        }
+        _logger.LogInformation($"{repo} is pending some fix. We will try to create\\update related pull request.");
+        await _workspaceManager.SetUserConfig(workPath, username: _githubUserDisplayName, email: _githubUserEmail);
+        var saved = await _workspaceManager.CommitToBranch(workPath, "Auto csproj fix and update by bot.", branch: _workingBranch);
+        if (!saved)
+        {
+            _logger.LogInformation($"{repo} has no suggestion that we can make. Ignore.");
+            return;
+        }
+
+        // Fork repo.
+        while ((await _gitHubService.GetRepos(_githubUserName)).All(r => r.Name != repo.Name))
+        {
+            await _gitHubService.ForkRepo(repo.Org, repo.Name);
+            // Wait a while. GitHub may need some time to fork the repo.
+            await Task.Delay(3000);
+        }
+
+        // Push to forked repo.
+        await _workspaceManager.Push(workPath, _workingBranch, $"https://{_githubUserName}:{_githubToken}@github.com/{_githubUserName}/{repo.Name}.git", force: true);
+
+        var existingPullRequestsByBot = await _gitHubService.GetPullRequest(repo.Org, repo.Name, head: $"{_githubUserName}:{_workingBranch}");
+        if (existingPullRequestsByBot.All(p => p.State != "open"))
+        {
+            // Create a new pull request.
+            await _gitHubService.CreatePullRequest(repo.Org, repo.Name, head: $"{_githubUserName}:{_workingBranch}", @base: repoDetails.DefaultBranch);
+        }
+        else
+        {
+            _logger.LogInformation($"Skipped creating new pull request for {repo} because there already exists.");
         }
     }
 }
